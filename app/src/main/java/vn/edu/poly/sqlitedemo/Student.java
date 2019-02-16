f533b03436db186d4f953519d26994dac0341037
HuyNguyen <huuhuybn@gmail.com>
2019-02-16 11:08:53 +0700

    cap nhat them sua xoa

---
+++ app/src/main/java/vn/edu/poly/sqlitedemo/Student.java
@@ -0,0 +1,11 @@
+package vn.edu.poly.sqlitedemo;
+
+class Student {
+
+    public String student_id;
+    public String student_name;
+    public String student_hometown;
+    public String student_birthday;
+    public String student_class_id;
+
+}
